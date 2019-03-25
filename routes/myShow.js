72c1be31591017b58e819cf251d3bf3a5c8064be
adam <378532514@qq.com>
2019-03-25 09:59:46 +0800

    myshow

--- routes/myShow.js
+++ routes/myShow.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const MyShowController = require('../controller/myShow');
+const MyShowController = require('../controller/myShow.js');
 const Joi = require('joi');
 const log = require('../utils/winston').getDefaultLogger;
 require('../utils/passport')(passport);
